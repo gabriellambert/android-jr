ef9e42ef852428b794cd18e18a3a9ac5bbd999ff
matheus <matheuslink18@hotmail.com>
2023-05-09 15:23:01 -0300

    Atualização das dependências do projeto, inclusão do arquivo v28/style.xml para Tab, inclusão de vetores com os icones de Tab e Fab, inclusão de cores, criação da activity Home

--- app/src/main/java/com/example/androidjr/MainActivity.kt
+++ app/src/main/java/com/example/androidjr/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.androidjr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnGoogle: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnGoogle = findViewById(R.id.btn_google)
+
+        btnGoogle.setOnClickListener{
+
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+
+        }
     }
 }
