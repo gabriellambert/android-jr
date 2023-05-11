7394aa128030943a93be5c56cc8de9e725d8d20d
colussicode <colussidev@gmail.com>
2023-05-11 10:16:17 -0300

    Main activity as first activity

--- app/src/main/java/com/example/androidjr/MainActivity.kt
+++ app/src/main/java/com/example/androidjr/MainActivity.kt
@@ -1,56 +1,26 @@
 package com.example.androidjr
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidjr.Adapter.Tab
-import com.example.androidjr.Fragments.BottomNavigation.HomeFragment
-import com.example.androidjr.Fragments.BottomNavigation.ListFragment
-import com.example.androidjr.Fragments.BottomNavigation.ProfileFragment
-import com.example.androidjr.databinding.ActivityHomeBinding
-import com.google.android.material.tabs.TabLayout
+import com.example.androidjr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityHomeBinding
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
 
-        supportActionBar?.hide()
-
-        setupTabLayout()
-        setupBottomNavigation()
+        goToHomeScreen()
     }
 
-    private fun setupBottomNavigation() {
-       binding.bottomNavigation.setOnNavigationItemReselectedListener { item ->
-            when(item.itemId) {
-                R.id.ic_roles -> HomeFragment()
-                R.id.ic_bookmarks -> ListFragment()
-                else -> ProfileFragment()
+    private fun goToHomeScreen() {
+        binding!!.btnGoogle.setOnClickListener {
+            Intent(this@MainActivity, HomeActivity::class.java).apply {
+                startActivity(this)
             }
         }
-    }
-
-    private fun setupTabLayout(){
-        binding.tabLayout.apply {
-            addTab(binding.tabLayout.newTab().setText("Home"))
-            addTab(binding.tabLayout.newTab().setText("Profile"))
-            addTab(binding.tabLayout.newTab().setText("Settings"))
-            tabGravity = TabLayout.GRAVITY_FILL
-        }
-        val adapter = Tab(supportFragmentManager, binding.tabLayout.tabCount)
-        binding.viewPager.adapter = adapter
-
-        binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
-        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                binding.viewPager.currentItem = tab!!.position
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
     }
 }
