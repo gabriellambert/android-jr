5ed025f4a6db978301fb543a339be79c641c9ea2
Vinicio Barbosa <Vinibza@gmail.com>
2023-06-06 15:12:39 +0100

    Finalizado a implementação do filtro de listagem de vagas.

--- app/src/main/java/com/example/androidjr/MainActivity.kt
+++ app/src/main/java/com/example/androidjr/MainActivity.kt
@@ -1,34 +1,32 @@
 package com.example.androidjr
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.androidjr.databinding.ActivityMainBinding
 import com.example.androidjr.home.domain.entity.params.RoleModel
-import com.example.androidjr.home.presentation.ui.RolesFragment
 import com.example.androidjr.home.presentation.adapter.ListItemsAdapter
+import com.example.androidjr.home.presentation.ui.RolesFragment
 import com.example.androidjr.home.tabs.presentation.adapter.TabAdapter
 import com.example.androidjr.profile.presentation.ui.ProfileFragment
 import com.example.androidjr.saveRoles.presentation.ui.SaveRolesFragment
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-
-    private val role = RoleModel()
-    private val adapter = ListItemsAdapter(context = this, items = role.items)
-
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val role = RoleModel()
+    private var adapterRecyclerView = ListItemsAdapter(context = this, items = role.items)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
-
         setupTabLayout()
         setupBottomNavigation()
         setupRecycleView()
     }
+
     private fun setupBottomNavigation() {
         binding.bottomNavigation.setOnNavigationItemReselectedListener { item ->
             when (item.itemId) {
@@ -38,6 +36,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun setupTabLayout() {
         binding.tabLayout.apply {
             addTab(binding.tabLayout.newTab().setText("Todos"))
@@ -46,22 +45,30 @@ class MainActivity : AppCompatActivity() {
             addTab(binding.tabLayout.newTab().setText("Flutter"))
             tabGravity = TabLayout.GRAVITY_FILL
         }
-        val adapter = TabAdapter(supportFragmentManager, binding.tabLayout.tabCount)
-        binding.viewPager.adapter = adapter
+
+        val adapterTablayout = TabAdapter(supportFragmentManager, binding.tabLayout.tabCount)
+        binding.viewPager.adapter = adapterTablayout
 
         binding.viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout))
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
                 binding.viewPager.currentItem = tab!!.position
+                adapterRecyclerView.filterList(tab.position)
+                adapterRecyclerView.notifyDataSetChanged()
             }
+
             override fun onTabUnselected(tab: TabLayout.Tab?) {
             }
+
             override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
+        }
+        )
     }
+
     private fun setupRecycleView() {
         val recyclerView = binding.recyclerView
         recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = adapter
+        recyclerView.adapter = adapterRecyclerView
     }
+
 }
